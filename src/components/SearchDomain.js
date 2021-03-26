ebffe9c449cb8730f4fc0d81502168113bf103fa
Alian Morales <amorales@sekureid.com>
2021-03-26 12:48:53 -0400

    added icon and yellow color to search btn

--- src/components/SearchDomain.js
+++ src/components/SearchDomain.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
@@ -12,7 +13,9 @@ export default function SearchDomain() {
       />
       <h4 className="input-domain">.health</h4>
       <InputGroup.Append>
-        <Button variant="primary">Search</Button>
+        <Button variant="warning">
+          <FiSearch />
+        </Button>
       </InputGroup.Append>
     </InputGroup>
   );
